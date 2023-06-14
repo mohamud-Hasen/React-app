71add7965a75c9dfed9fb090f176e3c1c848806f
Mohamud E Hasen <mohmudes4@gmail.com>
2023-06-14 12:44:37 -0500

    a simple react todo app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,91 @@
+
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>name: {props.name}</h1>
-    <h2>lastName: {props.lastName}</h2>
-    <h2>age: {props.age}</h2>
-    </>
-  )
-}
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-const  App = () => {
- 
+  useEffect(() => {
+    // Fetch todos from an API or local storage
+    // For simplicity, let's initialize with some dummy data
+    const initialTodos = [
+      { id: 1, text: 'Learn React', completed: false },
+      { id: 2, text: 'Build a todo app', completed: true },
+      { id: 3, text: 'Solve Algorithm Problems in Leetcode', completed: false },
+      { id: 4, text: 'Read Quran', completed: true },
+    ];
+
+    setTodos(initialTodos);
+  }, []);
+
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() === '') return;
+
+    const newTodoItem = {
+      id: Date.now(),
+      text: newTodo,
+      completed: false,
+    };
+
+    setTodos([...todos, newTodoItem]);
+    setNewTodo('');
+  };
+
+  const handleToggleTodo = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+
+    setTodos(updatedTodos);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <Person name="mohamud" lastName='hassan'age={38}  />
-      <Person name='alan' lastName='farah' age={28} />
-      <Person name='anfac' lastName='hassan' age={28}/>
-      <Person />
-      <Person />
-      
+    <div className="todo-app">
+      <h1>Mohamud's Todo Lists</h1>
+      <div className="add-todo">
+        <input
+          type="text"
+          value={newTodo}
+          onChange={handleInputChange}
+          placeholder="Enter a new todo"
+        />
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
+
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className={todo.completed ? 'completed' : ''}
+          >
+            <span>{todo.text}</span>
+            <div className="actions">
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleToggleTodo(todo.id)}
+              />
+              <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+
